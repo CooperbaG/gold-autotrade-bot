f71f136b8e0c7fb75f79bd98e4b0aaabc39db890
CooperbaG <beys.sarl@gmail.com>
2025-11-18 11:03:49 +0100

    uPDATE bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("Bot is running!")
+import telebot
+
+TOKEN = "TON_TOKEN_ICI"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Le bot fonctionne !")
+
+bot.polling()
